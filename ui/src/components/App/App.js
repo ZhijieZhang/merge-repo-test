22f4f0423105bfd5a79a0750c7de953267c56ac2
Zhijie Zhang <zhijiezhang0124@gmail.com>
2020-08-13 17:26:06 -0700

    test

--- ui/src/components/App/App.js
+++ ui/src/components/App/App.js
@@ -48,6 +48,8 @@ const propTypes = {
   removeEventHandlers: PropTypes.func.isRequired,
 };
 
+console.log('test');
+
 const App = ({ removeEventHandlers }) => {
   const store = useStore();
   const dispatch = useDispatch();
