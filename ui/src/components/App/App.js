05e8ef6c64e2a81c0442e3db0d340ed50217cb92
Zhijie Zhang <zhijiezhang0124@gmail.com>
2020-08-13 17:33:01 -0700

    hmm

--- ui/src/components/App/App.js
+++ ui/src/components/App/App.js
@@ -44,6 +44,7 @@ import actions from 'actions';
 
 import './App.scss';
 
+console.log('hmm');
 
 // TODO: Use constants
 const tabletBreakpoint = window.matchMedia('(min-width: 641px) and (max-width: 900px)');
